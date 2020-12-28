a034743525900587421408838281b090a15aea8d
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-12-28 14:24:38 +0530

    refactor: format files

--- compile.js
+++ compile.js
@@ -1,28 +1,27 @@
 const fs = require("fs");
-const path = require('path');
-const solc = require('solc');
- 
-const contractPath = path.resolve(__dirname,"contracts","lottery.sol");
+const path = require("path");
+const solc = require("solc");
 
-const source = fs.readFileSync(contractPath,"utf-8");
+const contractPath = path.resolve(__dirname, "contracts", "lottery.sol");
+
+const source = fs.readFileSync(contractPath, "utf-8");
 
 var input = {
-    language:'Solidity',
-    sources: {
-        'lottery.sol': {
-            content:source,
-        }
-    },
-    settings: {
-        outputSelection: {
-            "*": {
-                "*": ["*"]
-            }
-        }
-    }
+	language: "Solidity",
+	sources: {
+		"lottery.sol": {
+			content: source,
+		},
+	},
+	settings: {
+		outputSelection: {
+			"*": {
+				"*": ["*"],
+			},
+		},
+	},
 };
 
 var output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-module.exports=output.contracts['lottery.sol']['Lottery'];
-
+module.exports = output.contracts["lottery.sol"]["Lottery"];
--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,39 +1,40 @@
 pragma solidity >=0.7.0 <0.8.0;
 
 contract Lottery {
-    
     address public manager;
     address[] public players;
 
-    
     constructor() {
-        manager=msg.sender;
+        manager = msg.sender;
     }
-    
+
     function enter() public payable {
-        require(msg.value>.01 ether);
+        require(msg.value > .01 ether);
         players.push(msg.sender);
     }
-    
-    function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players)));
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            );
     }
-    
+
     function pickWinner() public restricted {
-        require(msg.sender==manager);
-        uint index=random()%players.length;
+        require(msg.sender == manager);
+        uint256 index = random() % players.length;
         payable(players[index]).transfer(address(this).balance);
-        players=new address[](0);
-        
+        players = new address[](0);
     }
-    
+
     modifier restricted() {
-        require(msg.sender==manager);
+        require(msg.sender == manager);
         _;
     }
-    
+
     function getPlayers() public view restricted returns (address[] memory) {
         return players;
     }
-    
 }
